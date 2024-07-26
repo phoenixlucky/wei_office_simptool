7c7d35d47c48c0d7056e5ee2942bc2f720aa0fa4
Ethan Wilkins <73684172+phoenixlucky@users.noreply.github.com>
2024-07-26 17:02:31 +0800

    Update mailManager.py
    优化邮件发送

--- wei_office_simptool/mailManager.py
+++ wei_office_simptool/mailManager.py
@@ -1,6 +1,9 @@
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import ssl
+import smtplib
+import datetime
 class eSend(object):
     """
     新邮件系统,可群发,群带附件
@@ -38,3 +41,34 @@ class eSend(object):
         except Exception as e:
             print(e)
             pass
+
+class DailyEmailReport:
+    def __init__(self, email_host, email_port, email_username, email_password):
+        self.email_host = email_host
+        self.email_port = email_port
+        self.email_username = email_username
+        self.email_password = email_password
+        self.receivers = []
+        self.msg = MIMEMultipart()
+
+    def add_receiver(self, receiver_email):
+        self.receivers.append(receiver_email)
+
+    def set_email_content(self, subject, body):
+        self.msg['From'] = self.email_username
+        self.msg['To'] = ', '.join(self.receivers)
+        self.msg['Subject'] = subject
+        self.msg.attach(MIMEText(body, 'plain'))
+
+    def send_email(self):
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.email_host, self.email_port, context=context) as server:
+            server.login(self.email_username, self.email_password)
+            server.sendmail(self.email_username, self.receivers, self.msg.as_string())
+            print('邮件发送成功！')
+
+    def send_daily_report(self,title,text):
+        subject = f'{title} - {datetime.date.today()}'
+        body = text
+        self.set_email_content(subject, body)
+        self.send_email()
